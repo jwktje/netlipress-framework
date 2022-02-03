a1e7e23374fc286d6b7e31cff26bde0e15e667e8
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-03 12:35:21 +0100

    Support for reusable field groups

--- src/classes/NetlifyCms.php
+++ src/classes/NetlifyCms.php
@@ -4,20 +4,39 @@ namespace Netlipress;
 
 class NetlifyCms
 {
-    private static function getConfig()
+    private static function getConfig($includeFieldGroups = true)
     {
-        return json_decode(file_get_contents(APP_ROOT . '/web/admin/config.json'));
+        $configFile = APP_ROOT . '/web/admin/config.json';
+        $fieldsFile = APP_ROOT . '/web/admin/fields.json';
+        $config = json_decode(file_get_contents($configFile));
+
+        if($includeFieldGroups) {
+            if(file_exists($fieldsFile)) {
+                $fieldsConfig = json_decode(file_get_contents($fieldsFile));
+                //Copy shared fields to all relevant collections
+                foreach($fieldsConfig ?? [] as $sharedFieldGroup) {
+                    foreach($sharedFieldGroup->collections ?? [] as $collection) {
+                        $collectionIndex = array_search($collection, array_column($config->config->collections, 'name'));
+                        if($collectionIndex !== false) {
+                            //We found a matching collection in the NetlifyCMS config so we should fill the fields from our shared group
+                            $config->config->collections[$collectionIndex]->fields = $sharedFieldGroup->fields;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $config;
     }
 
     public static function render()
     {
-        $minifiedNetlifyCmsConfig = json_encode(self::getConfig());
         include(__DIR__ . './../includes/templates/netlify-cms.php');
     }
 
     public static function usesNetlifyIdentity()
     {
-        $config = self::getConfig();
+        $config = self::getConfig(false);
         return isset($config->config->backend->name) && $config->config->backend->name === 'git-gateway';
     }
 
