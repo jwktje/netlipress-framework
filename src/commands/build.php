8a593224048694453b33217514ee653bc489a9bb
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-03 16:34:02 +0100

    Changed build command config loading path

--- src/commands/build.php
+++ src/commands/build.php
@@ -2,7 +2,7 @@
 /*
  * Run this command with PHP from the commandline from the root of your project to generate a static version in a "build" directory
  */
-require './config.php';
+require './config/netlipress.php';
 require './vendor/autoload.php';
 $ssg = new Netlipress\StaticSite();
 $ssg->generate();
