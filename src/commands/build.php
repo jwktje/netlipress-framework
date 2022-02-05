9f7889ca77bdb6bb4d29b3e1d61cf222b6892f6f
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-05 16:13:25 +0100

    Added Mix skipping

--- src/commands/build.php
+++ src/commands/build.php
@@ -1,8 +1,13 @@
 <?php
-/*
- * Run this command with PHP from the commandline from the root of your project to generate a static version in a "build" directory
- */
 require './config/netlipress.php';
 require './vendor/autoload.php';
+
+//Only run Laravel Mix when committing yourself, not the CMS. Use [skip-mix] if you want to also skip it on your own commit
+exec(" git log -1 --pretty=%B", $commitMessage);
+if(isset($commitMessage[0]) && str_contains($commitMessage[0], '[skip-mix]')) {
+    exec('npm run prod');
+}
+
+//Run the static site generator
 $ssg = new Netlipress\StaticSite();
 $ssg->generate();
