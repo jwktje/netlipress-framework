7f52bb0c9d7f40442df0f9b11c7ccde97192274c
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-02-05 16:20:32 +0100

    Fixed conditional

--- src/commands/build.php
+++ src/commands/build.php
@@ -4,7 +4,7 @@ require './vendor/autoload.php';
 
 //Only run Laravel Mix when committing yourself, not the CMS. Use [skip-mix] if you want to also skip it on your own commit
 exec(" git log -1 --pretty=%B", $commitMessage);
-if(isset($commitMessage[0]) && str_contains($commitMessage[0], '[skip-mix]')) {
+if(isset($commitMessage[0]) && !str_contains($commitMessage[0], '[skip-mix]')) {
     exec('npm run prod');
 }
 
