caeecc7bba5c211f67d864a372b485ffde67b577
Jan Willem Kolkman <jwkolkman@gmail.com>
2022-04-19 22:15:13 +0200

    Configurable attr for media

--- src/includes/tags/wp_media.php
+++ src/includes/tags/wp_media.php
@@ -9,20 +9,36 @@ function wp_get_attachment_image_uri($url)
     return $url;
 }
 
-function wp_get_attachment_image($url, $size = 'full')
+function wp_get_attachment_image($url, $size = 'full', $icon = false, $attr = [])
 {
     if (!$url) return;
     $url = wp_get_attachment_image_uri($url);
-    $alt = basename($url);
+    ;
 
     if ($size == 'thumb') {
         $url .= '?size=150x150';
         $sizes = 'width="150" height="150"';
     } else {
-        $sizes = getimagesize(APP_ROOT . PUBLIC_DIR . $url);
+        $sizes = getimagesize(APP_ROOT . PUBLIC_DIR . $url)[3];
     }
 
-    return "<img src='$url' alt='$alt' $sizes[3] loading='lazy'/>";
+    $defaultsAttr = [
+        'class' => '',
+        'loading' => 'lazy',
+        'alt' => $alt = basename($url)
+    ];
+
+    $attr = array_merge(
+        $defaultsAttr,
+        array_intersect_key($attr, $defaultsAttr)
+    );
+
+    $html = "<img src='$url' $sizes";
+    foreach($attr as $name => $value) {
+        $html .= " $name=" . '"' . $value . '"';
+    }
+    $html .= "/>";
+    return $html;
 }
 
 function output_link($link, $class = '')
